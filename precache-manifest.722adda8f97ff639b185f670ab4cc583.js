a4db34c8e9566bf1acb78cb00a0c2f0b83c8adcf
nalinikanta123 <nalinikantam@gmail.com>
2020-09-24 02:20:55 -0400

    Updates

---
+++ precache-manifest.722adda8f97ff639b185f670ab4cc583.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd8ef33542f6aa321d7db7b1c42edd62",
+    "url": "/simple-todo-app/index.html"
+  },
+  {
+    "revision": "724b76181b22f8dafb7d",
+    "url": "/simple-todo-app/static/js/2.5b7308f3.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/simple-todo-app/static/js/2.5b7308f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd4f053b2293b7a4d38d",
+    "url": "/simple-todo-app/static/js/main.6e9194be.chunk.js"
+  },
+  {
+    "revision": "854a73988d019f913f52",
+    "url": "/simple-todo-app/static/js/runtime-main.01792ca7.js"
+  }
+]);
